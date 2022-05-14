5c4296045520b757b335b6e9a296225216f0d4dd
IrinaSaba <sabaliauskieneirina@gmail.com>
2022-05-14 20:55:12 +0000

    Deploying to gh-pages from @ IrinaSaba/TranslimitasKelyje@57573efa29536aaa5a9eab75435d69f5aed71669 🚀

--- src.e27da47a.js
+++ src.e27da47a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/TranslimitasKelyje/src.2bf867f3.js.map
+//# sourceMappingURL=/TranslimitasKelyje/src.e27da47a.js.map
